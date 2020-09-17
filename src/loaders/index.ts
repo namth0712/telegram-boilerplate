2bcaf76b2bd8f3522917af31b4915f70f7f4cbd3
NamTH <namth0712@gmail.com>
2020-09-17 16:24:44 +0700

    Add corona command

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,11 +1,13 @@
 import telegramLoader from './telegram';
 import general from '../apps/general';
+import corona from '../apps/corona';
 
 export default async () => {
   const bot = telegramLoader();
   console.info('✌️ Telegram loaded');
 
   general(bot);
+  corona(bot);
 
   return bot;
 };
