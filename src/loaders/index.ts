287f1e593b2147433b7110056db0851d678859f2
NamTH <namth0712@gmail.com>
2020-09-17 15:55:31 +0700

    Remove corona example

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,13 +1,11 @@
 import telegramLoader from './telegram';
 import general from '../apps/general';
-import corona from '../apps/corona';
 
 export default async () => {
   const bot = telegramLoader();
   console.info('✌️ Telegram loaded');
 
   general(bot);
-  corona(bot);
 
   return bot;
 };
